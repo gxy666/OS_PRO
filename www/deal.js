beb0f5ccf4373f1e57b9876cc0e9b2d94d23b7fb
gxy666 <13998880632@163.com>
2020-01-07 15:59:58 +0800

    test

--- www/deal.js
+++ www/deal.js
@@ -6,7 +6,7 @@ var operaton_num;
 var operation_array;
 var instruction;
 var data_whole = new Object();
-var current_speed = 3;
+var current_speed = 3000;//数值越大速度越慢
 function sleep(d){
 	for(var t = Date.now();Date.now() - t <= d;);
 }
@@ -14,60 +14,123 @@ function sleep(d){
 function set_speed(speed){
 	current_speed = speed;
 }
-function display_animation(button_type)
+
+function display_animation(i)
 {
-	if(button_type==1)
+	//alert(data_whole.length);
+	type_animation = data_whole[i]["type_animation"];
+	background_animation = data_whole[i]["background_animation"];
+	var_num = data_whole[i]["var_num"];
+	var_array = data_whole[i]["var_array"];
+	operaton_num = data_whole[i]["operation_num"];
+	operation_array = data_whole[i]["operation_array"];
+	instruction = data_whole[i]["instruction"];
+	switch(type_animation)
 	{
-		$.each(data_whole, function (i, item) {
-			type_animation = item["type_animation"];
-			background_animation = item["background_animation"];
-			var_num = item["var_num"];
-			var_array = item["var_array"];
-			operaton_num = item["operation_num"];
-			operation_array = item["operation_array"];
-			instruction = item["instruction"];
-			alert(type_animation);//显示处理后的数据
-		});
-		anime({
-			targets: '#main',
-			translateX: 2000,
-			duration:1000*current_speed,
-			complete: function() {
-				//$("#main").remove();
-				$('#main').css('display','none');
-			}
-		});
-		  
+		case 1:
+		{
+			
+			break;
+		}
+		case 2:
+		{
+			
+			break;
+		}
+		case 3:
+		{
+		
+			break;
+		}
+		case 4:
+		{
+			
+			break;
+		}
+		case 5:
+		{
+			
+			break;
+		}
+		case 6:
+		{
+			//alert(type_animation);
+			$('#main').css('display','block');
+			document.getElementById("content").innerHTML=instruction;
+			$('#bt1').css('display','none');
+			$('#bt2').css('display','none');
+			anime({
+				targets: '#main',
+				translateX: 0,
+				duration: current_speed,
+				complete: function()
+				{
+					anime({
+						targets: '#main',
+						translateX: 2000,
+						duration: current_speed,
+						delay: current_speed/3,
+						complete: function()
+						{
+							$('#main').css('display','none');
+							if(i+1<data_whole.length)
+							{
+								display_animation(i+1);
+							}
+						}
+					});
+				}
+			});
+			break;
+		}
 	}
-	else if(button_type==2)
-	{
-		$.each(data_whole, function (i, item) {
-			type_animation = item["type_animation"];
-			background_animation = item["background_animation"];
-			var_num = item["var_num"];
-			var_array = item["var_array"];
-			operaton_num = item["operation_num"];
-			operation_array = item["operation_array"];
-			instruction = item["instruction"];
-			alert(instruction);
-		});
-	}
+
 }
 
 $(document).ready(function(){
 	$("#bt1").click(function () {
 		$.getJSON("test1.json", function (data) {
 			data_whole = data;
-			display_animation(1);
+			anime({
+				targets: '#main',
+				translateX: 2000,
+				duration: current_speed,
+				complete:function(){
+					$('#main').css('display','none');
+					display_animation(0);
+				}
+			});
 		});
-		//alert("接下来将演示一个执行shell脚本的过程");//显示处理后的数据
 		
 	});
+
 	$("#bt2").click(function () {
-		$.getJSON("test1.json", function (data) {
+		$.getJSON("test2.json", function (data) {
 			data_whole = data;
+			anime({
+				targets: '#main',
+				translateX: 2000,
+				duration: current_speed,
+				complete:function(){
+					$('#main').css('display','none');
+					display_animation(0);
+				}
+			});
 		});
-		alert("接下来将演示一个执行可执行文件的过程");//显示处理后的数据
-		display_animation(2);
+	});
+
+	$("#sb1").click(function () {
+		set_speed(3000);
+		//alert(current_speed);
+	});
+
+	$("#sb2").click(function () {
+		set_speed(1500);
+		//alert(current_speed);
+	});
+
+	$("#sb3").click(function () {
+		set_speed(6000);
+		//alert(current_speed);
 	});
 });
